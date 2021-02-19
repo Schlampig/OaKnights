3cfb9e7e2ac5ed5e8c997e528b757db1493527e7
Kuyi King <kuyiking@gmail.com>
2021-02-19 15:47:47 +0800

    Update add_operator_relation.py

--- OperatorGraph/add_operator_relation.py
+++ OperatorGraph/add_operator_relation.py
@@ -22,6 +22,7 @@ def add_operator_relation(load_entity, load_relation, load_cp, save_relation):
             if code_now not in d_operator.keys():
                 d_operator[code_now] = str(id_now)
     # 在关系表后面拼接新关系
+    lst_type = list()  # 记录人际关系类型
     df_relation = pd.read_csv(load_relation)
     df_cp = pd.read_excel(load_cp)
     for i_cp in tqdm(range(df_cp.shape[0])):
@@ -34,12 +35,47 @@ def add_operator_relation(load_entity, load_relation, load_cp, save_relation):
                 r_now = pd.DataFrame({":START_ID(node)": [head_id], ":END_ID(node)": [tail_id],
                                       ":TYPE": [relation_h2t], "name": [relation_h2t]})
                 df_relation = df_relation.append(r_now)
+                lst_type.append(relation_h2t)
             if relation_t2h != "无":
                 r_now = pd.DataFrame({":START_ID(node)": [tail_id], ":END_ID(node)": [head_id],
                                       ":TYPE": [relation_t2h], "name": [relation_t2h]})
                 df_relation = df_relation.append(r_now)
+                lst_type.append(relation_t2h)
     # 存储新关系
     df_relation.to_csv(save_relation, index=False, encoding="utf-8-sig")
+
+    # 打印示例查询语句
+    lst_type = list(set(lst_type))  # type类型去重
+    print_type = "|".join(["`" + t + "`" for t in lst_type])
+    print()
+
+    print("说明：匹配的基本语法结构为：MATCH path=()-[]->() WHERE y RETURN x LIMIT z")
+    print("其中，()内写点的类型，[]内写边的类型，y控制类型的属性，x表示想要返回的结果，z表示对结果数量的限制。")
+    print()
+
+    print("示例1：显示指定干员A的所有信息")
+    print("格式：MATCH path=(n:中文代号)<--() WHERE n.name=\"A\" RETURN path")
+    print("A=刻刀：MATCH path=(n:中文代号)<--() WHERE n.name=\"刻刀\" RETURN path")
+    print()
+
+    print("示例2：显示满足R属性的N名干员信息, 若需显示该属性下所有干员信息，则去掉LIMIT N")
+    print("格式：MATCH path=()-[r:`R`]->() RETURN path LIMIT N")
+    print("R=星座，N=15：MATCH path=()-[r:`星座`]->() RETURN path LIMIT 15")
+    print()
+
+    print("示例3：显示类型R中属性为X的所有干员")
+    print("格式：MATCH path=(n:R {name:\"X\"})-[]->() RETURN path")
+    print("R=种族，X=萨卡兹：MATCH path=(n:种族 {name:\"萨卡兹\"})-[]->() RETURN path")
+    print()
+
+    print("示例4：查找干员A与干员B之间的最短路径：")
+    print("格式：MATCH path=shortestpath((n:中文代号)-[r:`活跃领域`|" + print_type + "*]->(m:中文代号)) WHERE n.name=\"A\" AND m.name=\"B\" RETURN path")
+    print("A=温蒂，B=初雪：MATCH path=shortestpath((n:中文代号)-[r:`活跃领域`|" + print_type + "*]->(m:中文代号)) WHERE n.name=\"星熊\" AND m.name=\"塞雷娅\" RETURN path")
+    print()
+
+    print("灵活配置脚本，探索更灵活的查询结果吧！")
+    print("注：1）适当设置LIMIT防止查询节点过大卡机（虽然目前约200干员影响不大）；2）查询未必有结果，可到Issue中留言。")
+
     print("- 执行完毕 -")
     return None
 
